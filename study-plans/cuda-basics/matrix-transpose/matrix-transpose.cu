6f85f5b85782faaa6f379025ba4cf26ddb40cedc
Andrew Loo <82577844+wowthecoder@users.noreply.github.com>
2026-06-14 19:38:56 +0100

    Solve matrix-transpose

---
+++ study-plans/cuda-basics/matrix-transpose/matrix-transpose.cu
@@ -0,0 +1,17 @@
+#include <cuda_runtime.h>
+
+__global__ void matrix_transpose_kernel(const float* A, float* B, int M, int N) {
+    int j = blockIdx.x * blockDim.x + threadIdx.x;
+    int i = blockIdx.y * blockDim.y + threadIdx.y;
+
+    if (i < M && j < N) {
+        B[j*M + i] = A[i*N + j];
+    }
+}
+
+extern "C" void solve(const float* A, float* B, int M, int N) {
+    dim3 threads(16, 16);
+    dim3 blocks((N + 15) / 16, (M + 15) / 16);
+    matrix_transpose_kernel<<<blocks, threads>>>(A, B, M, N);
+    cudaDeviceSynchronize();
+}
